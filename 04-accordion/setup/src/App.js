730a14e0ae3435898c78274fceef7f9a67819741
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2023-12-15 00:28:18 +0530

    project04-completed

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -2,7 +2,22 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [dataPoint, setDataPoint] = useState(data)
+  // const elem
+  console.log(dataPoint)
+  // const elem = dataPoint.map(console.log(`boi`))
+  return (
+    <main>
+      <div className='container'>
+        <h3>Questions and answers about login </h3>
+        <section className='info'>
+          {
+            dataPoint.map(question => <SingleQuestion key={question.id} {...question}/>)
+          }
+        </section>
+      </div>
+    </main>
+    );
 }
 
 export default App;
