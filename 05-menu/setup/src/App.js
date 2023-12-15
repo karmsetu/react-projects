a64ac0cc7d2bf5a129a1f328e4fe9ea41d28bc9c
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2023-12-15 17:43:05 +0530

    project04-completed

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -4,7 +4,30 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items)
+  const [categories, setCategories] = useState([])
+  const uniqueCategories =()=> {
+    const set = new Set()
+    items.map(item=> set.add(item.category))
+    return set
+  }
+  const catSend = uniqueCategories()
+  const filterItems =(str)=> {
+    const filteredItem = items.filter(item => item.category === str )
+    setMenuItems(filteredItem)
+  }
+  return(
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h3>Our Menu</h3>
+          <div className='underline'></div>
+        </div>
+        <Categories filterItems={filterItems} catSend={catSend}/>
+        <Menu items={menuItems}/>
+      </section>
+    </main>  
+  );
 }
 
 export default App;
--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({filterItems, catSend}) => {
+  const elem = []
+  catSend.forEach(element => {
+    const single = <button className='filter-btn' onClick={()=> filterItems(element)} key={element}>{element}</button>
+    elem.push(single)
+  });
+  
+  return (
+    <div className='btn-container'>
+      {elem}
+    </div>
+    );
 };
 
 export default Categories;
