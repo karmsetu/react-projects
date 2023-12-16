1b0f8e0de6317cfe42e8d101cd33e573f1aa7042
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2023-12-16 13:39:23 +0530

    project07-completed

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,10 +1,40 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState([])
+  const handleSubmit =(e)=> {
+    e.preventDefault()
+    const amount = parseInt(count)
+    if (amount > data.length) {
+      setCount(data.length)
+    }
+
+    setText(data.slice(0,amount))
+    console.log(`hello`)
+  }
   
-  
+  const generatePara =()=> {
+  }
   return (
-  <h2>lorem ipsum project setup</h2>
+  <section className='section-center'>
+    <h3>tired of boring lorem ipsum</h3>
+    <form className='lorem-form' onSubmit={handleSubmit} action="">
+      <label htmlFor="amount">
+        paragraphs:
+      </label>
+      <input type="number" name="amount" id="amount" value={count} onChange={(e)=>{
+        e.preventDefault()
+        setCount(e.target.value)
+      }}/>
+      <button type="submit" className='btn' onClick={generatePara}> generate </button>
+    </form>
+    <article className="lorem-text">
+      {
+        text.map((para, index)=> <p key={index}>{para}</p>)
+      }
+    </article>
+  </section>
     )
 }
 
