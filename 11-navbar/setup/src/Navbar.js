42d58701baa8859ca87ae01a5120a8743baa542b
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2023-12-18 01:18:01 +0530

    project11-completed-need to redo ref hooks

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -4,7 +4,54 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  const [linkData, setLinkData] = useState(links)
+  const [socialData, setSocialData] = useState(social)
+  const [showLinks, setShowLinks] = useState(false)
+  const linksContainerRef = useRef(null)
+  const linksRef = useRef(null)
+  useEffect(()=> {
+    const linksHeight = linksRef.current.getBoundingClientRect().height
+    console.log(linksHeight)
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`
+    } else {
+      linksContainerRef.current.style.height = `0px`
+    }
+  }, [showLinks])
+
+  return (
+    <div className="nav-center">
+      <div className="nav-header">
+        <img src={logo} alt="logo" />
+        <button className='nav-toggle' onClick={()=> setShowLinks(old => !old)}>
+          <FaBars/>
+        </button>
+      </div>
+      
+      <div className={`links-container`} ref={linksContainerRef}>
+        <ul className="links" ref={linksRef}>
+          {
+            linkData.map(item => {
+              const {id, url, text} = item
+              return <li key={id}> <a href={url}>{text}</a> </li>
+          })
+          }
+        </ul>
+      </div>
+      
+      <ul className="social-icons">
+        {
+          socialData.map(item => {
+            const {id, url, icon} = item
+            return <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          })
+        }
+        
+      </ul>
+    </div>
+    )
 }
 
 export default Navbar
